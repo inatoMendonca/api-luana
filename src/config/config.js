b582d3c7c1e06831282eb329b7cc23dc4cdf198c
Renato Jean V. Christino <48481257+inatoMendonca@users.noreply.github.com>
2020-01-15 09:18:24 -0300

    vai malandra

--- src/config/config.js
+++ src/config/config.js
@@ -13,6 +13,7 @@ module.exports =  {
         database: {
             host: 'us-cdbr-iron-east-05.cleardb.net',
             port: process.env.PORT,
+            name: 'heroku_f8b2d4ffd842016',
             dialect: 'mysql',
             user: 'b845bb0ac077fc',
             password: '2472749d'
