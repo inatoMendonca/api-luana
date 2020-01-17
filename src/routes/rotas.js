9d64fa6c05d79aefb3525818f58b18c39072a90a
Renato Jean V. Christino <48481257+inatoMendonca@users.noreply.github.com>
2020-01-17 10:57:42 -0300

    caralho

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -10,7 +10,7 @@ const router = express();
 
 console.log('Enabling CORS');
 let allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://luana-mansueli.herokuapp.com/home');
+    res.header('Access-Control-Allow-Origin', 'https://luana-api.herokuapp.com/api/agendamentos');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     if ('OPTIONS' === req.method) {
